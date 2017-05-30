2cb849ee04d6944bc34a9b48ff67f618a816a981
whatarule <whatarule@gmail.com>
2017-05-30 16:51:15 +0900

    Parsec

--- hutton/src/Parsec.hs
+++ hutton/src/Parsec.hs
@@ -4,18 +4,31 @@ module Parsec (
 
 --import Text.Parsec.Prim
 --import Text.Parsec.Combinator
+--import Text.Parsec.String (Parser)
 
-
-import Text.Parsec (parse, many, count, sepBy)
-import Text.Parsec.String (Parser)
-import Text.Parsec.Char (anyChar, letter, digit)
+import Text.Parsec (Parsec, ParseError, parse, many, count, sepBy, (<|>))
+import Text.Parsec.Char (noneOf, anyChar, alphaNum, letter, digit)
 import Text.Parsec.Language (haskellDef)
 import qualified Text.Parsec.Token as T
 
 lexer = T.makeTokenParser haskellDef
 
-symbol = T.symbol lexer
+commaSep = T.commaSep lexer
+semiSep = T.semiSep lexer
+
+parens = T.parens lexer
+
+identifier = T.identifier lexer
 natural = T.natural lexer
+stringLiteral = T.stringLiteral lexer
+symbol = T.symbol lexer
+
+-- |
+-- >>> parse (symbol "(" *> sepBy (many alphaNum) (symbol ",") <* symbol ")") "" "(name,aaa,bbb)"
+-- Right ["name","aaa","bbb"]
+-- >>> parse (symbol "(" *> sepBy (many alphaNum) (symbol ",") <* symbol ")") "" "(1,name,aaa)"
+-- Right ["1","name","aaa"]
+--
 
 -- |
 -- >>> parse digit "" "1"
@@ -32,6 +45,7 @@ natural = T.natural lexer
 -- Right "1"
 -- >>> parse (pure show <*> natural <* symbol ",") "" "1,name"
 -- Right "1"
+--
 -- >>> parse (symbol "name") "" "name"
 -- Right "name"
 -- >>> parse (many letter) "" "name"
@@ -44,6 +58,7 @@ natural = T.natural lexer
 -- Right "2"
 -- >>> parse ((++) <$> (show <$> natural <* symbol ",") <*> (show <$> natural)) "" "1,2"
 -- Right "12"
+--
 -- >>> (flip (:) []) "aaa"
 -- ["aaa"]
 -- >>> reverse . flip (:) ["aaa"] $ "bbb"
@@ -56,6 +71,7 @@ natural = T.natural lexer
 -- Right ["1"]
 -- >>> parse (fmap show <$> many natural <* symbol ",") "" "1,2"
 -- Right ["1"]
+--
 -- >>> parse ((++) <$> (flip (:) [] . show <$> natural <* symbol ",") <*> (flip (:) [] . show <$> natural)) "" "1,2"
 -- Right ["1","2"]
 -- >>> parse ((++) <$> (flip (:) [] . show <$> natural <* symbol ",") <*> (flip (:) [] <$> many letter)) "" "1,name"
@@ -69,6 +85,65 @@ natural = T.natural lexer
 -- >>> parse ((++) <$> (symbol "(" *> (fmap show <$> (many natural)) <* symbol ",") <*> sepBy (many letter) (symbol ",") <* symbol ")") "" "(1,name,aaa)"
 -- Right ["1","name","aaa"]
 --
+-- >>> parse (many alphaNum) "" "a01"
+-- Right "a01"
+-- >>> parse (many identifier) "" "a01"
+-- Right ["a01"]
+-- >>> parse (many stringLiteral) "" "a01"
+-- Right []
+-- >>> parse (many stringLiteral) "" "aaa"
+-- Right []
+-- >>> parse (stringLiteral) "" "\"aaa\""
+-- Right "aaa"
+--
+-- >>> parse (symbol "(" *> sepBy (many identifier) (symbol ",") <* symbol ")") "" "(name,aaa,bbb)"
+-- Right [["name"],["aaa"],["bbb"]]
+-- >>> parse (symbol "(" *> sepBy identifier (symbol ",") <* symbol ")") "" "(name,aaa,bbb)"
+-- Right ["name","aaa","bbb"]
+--
+-- >>> parse (symbol "(" *> sepBy (many alphaNum) (symbol ",") <* symbol ")") "" "(name,aaa,bbb)"
+-- Right ["name","aaa","bbb"]
+-- >>> parse (symbol "(" *> sepBy (many alphaNum) (symbol ",") <* symbol ")") "" "(1,name,aaa)"
+-- Right ["1","name","aaa"]
+--
+-- >>> parse (symbol "(" *> commaSep (many alphaNum) <* symbol ")") "" "(name,aaa,bbb)"
+-- Right ["name","aaa","bbb"]
+-- >>> parse (symbol "(" *> commaSep (many alphaNum) <* symbol ")") "" "(1,name,aaa)"
+-- Right ["1","name","aaa"]
+--
+-- >>> parse (symbol "(" *> commaSep (many alphaNum) <* symbol ")") "" "(1, name, aaa)"
+-- Right ["1","name","aaa"]
+--
+-- >>> parse (parens $ many alphaNum) "" "(1)"
+-- Right "1"
+-- >>> parse (parens . commaSep $ many alphaNum) "" "(1,name)"
+-- Right ["1","name"]
+-- >>> parse (semiSep . many $ noneOf ";") "" "select;(1,name)"
+-- Right ["select","(1,name)"]
+--
+-- >>> sequenceA . fmap (parse ((parens . commaSep $ many alphaNum) <|> many identifier) "") =<< parse (semiSep . many $ noneOf ";") "" "select;(1,name)"
+-- Right [["select"],["1","name"]]
+-- >>> sequenceA . fmap (parse ((parens . commaSep $ many alphaNum) <|> many identifier) "") =<< parse (semiSep . many $ noneOf ";") "" "select;(id,field);(1,name)"
+-- Right [["select"],["id","field"],["1","name"]]
+--
+
+parsecInputString :: Parsec String () [String]
+-- |
+-- >>> parse parsecInputString "" "(1,name,aaa)"
+-- Right ["1","name","aaa"]
+-- >>> parse parsecInputString "" "(A01,name,aaa)"
+-- Right ["A01","name","aaa"]
+parsecInputString = parens . commaSep $ many alphaNum
+
+
+parseInputString :: String -> Either ParseError [String]
+-- |
+-- >>> parseInputString "(1,name,aaa)"
+-- Right ["1","name","aaa"]
+-- >>> parseInputString "(A01,name,aaa)"
+-- Right ["A01","name","aaa"]
+parseInputString = parse parsecInputString msg
+  where msg = ""
 
 -- >>> splitOn "," "1,name"
 -- ["1","name"]
@@ -80,7 +155,6 @@ natural = T.natural lexer
 --
 
 
-p :: Parser String
 p = foldr (++) <$> symbol "a" <*> many (symbol "a")
 
 -- |
